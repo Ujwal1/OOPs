d5343fbe02ec15b78f1f4816c6c8108ee16da07d
Ujwal Guptya <38862638+Ujwal1@users.noreply.github.com>
2019-03-17 21:18:04 +0530

    Update 01-EmployeeStructure.cpp
    a structure Employee with members: empid, name, age, salary and address. Define functions
    •	to enter the record for 5 employees, 
    •	to display the record of all the employees.
    •	to search and display the record by name as well as by empid. 
    •	to update the salary of the employee.

--- 01-EmployeeStructure.cpp
+++ 01-EmployeeStructure.cpp
@@ -1,3 +1,10 @@
+/*
+a structure Employee with members: empid, name, age, salary and address. Define functions
+•	to enter the record for 5 employees, 
+•	to display the record of all the employees.
+•	to search and display the record by name as well as by empid. 
+•	to update the salary of the employee. 
+*/
 #include <iostream>
 #include <malloc.h>
 #include <strings.h>
